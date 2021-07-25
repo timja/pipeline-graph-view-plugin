a3da2b9784ef54f607c13ab6e0ece998968b9bb9
Tim Jacomb <timjacomb1+github@gmail.com>
2021-07-25 19:56:30 +0100

    Colours

--- src/main/frontend/pipeline-console-view/app.tsx
+++ src/main/frontend/pipeline-console-view/app.tsx
@@ -1,11 +1,7 @@
 import React from "react";
-//import ReactDOM from "react-dom";
 import { FunctionComponent } from 'react';
 
 import { PipelineConsole } from './pipeline-console/main/';
-
-//const rootElement = document.getElementById("root");
-//ReactDOM.render(<DataTreeView />, rootElement);
 
 const App: FunctionComponent = () => {
 
